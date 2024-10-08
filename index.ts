932bab9075e0007ed0698083bd6748f4952502dc
DucTran2310 <trananhduc23102000@gmail.com>
2024-10-08 16:32:22 +0700

    Update connect mongoDB

--- index.ts
+++ index.ts
@@ -1,16 +1,31 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 dotenv.config()
 
-const dbUrl = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.z9tcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.z9tcw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 const app = express()
 
 const PORT = process.env.PORT || 8081;
 
-app.listen(PORT, () => {
-  console.log(`:::Server is starting at http://localhost:${PORT}`)
-}).on('error', (err: any) => {
-  console.log(':::ERROR: ', err)
-  throw new Error(err)
-})
+const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURL);
+    console.log(':::Connected to MongoDB successfully!');
+  } catch (error) {
+    console.error(':::Error connecting to MongoDB:', error);
+    process.exit(1);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`:::Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(':::Server startup failed:', error);
+  });
